469e8ed4293c8a02ef6800f78c95475b09f70ed4
Tommy <tommy.crestanello@digitaslbi.com>
2017-08-02 17:43:18 +0200

    stingrayService javascript separation

---
+++ src/_js/stingrayService.js
@@ -0,0 +1,12 @@
+/**
+ * stingrayService : definintion of the services for the platform
+ */
+
+// This service must be reconsidered because actually doesn't work correctly
+// TODO: linking to a databse nosql ( json ) that permit to recover all data
+// and put them on the UI.
+stringrayApp.service('translations', [ '$http', function($http){
+  var obj = {};
+  return obj;
+
+}]);
